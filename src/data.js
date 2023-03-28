4f08216397fbc532c4bfdbd3d104f88209355e20
1205324997 <josi.i.dami@gmail.com>
2023-03-27 20:06:40 -0500

    filtrado de selector pokemon

--- src/data.js
+++ src/data.js
@@ -1,6 +1,8 @@
-// estas funciones son de ejemplo
-
-function Pokemon(filterPokemon){
-  const filtrar = 
-
-}
+export const filterPokemon = (data, pokemonType) => {
+  // puedes borrar/comentar los console.logs de aqui
+ return data.filter(item => item.type == pokemonType);
+ console.log(pokemonType)
+// aqui escribes el filtrado
+  
+// aqui retornas el resultado de los pokemones filtrados
+};
--- src/main.js
+++ src/main.js
@@ -1,6 +1,35 @@
-import { example } from './data.js';
-// import data from './data/lol/lol.js';
 import data from './data/pokemon/pokemon.js';
-// import data from './data/rickandmorty/rickandmorty.js';
+import { filterPokemon} from './data.js';
 
-console.log(example, data);
+
+const root = document.getElementById("root")
+
+function verType(){
+data.pokemon.forEach((pokemones)=>{
+    const section = document.createElement("section");
+    const subtitle = document.createElement("h2");
+    const imgPokemon = document.createElement("img");
+    
+    
+    
+    subtitle.textContent = pokemones.name;
+    imgPokemon.setAttribute('src', pokemones.img);
+
+   
+
+    section.appendChild(imgPokemon);
+    section.appendChild(subtitle);
+    root.appendChild(section)
+
+})
+}
+
+
+const typePokemon = document.getElementById ("type")
+typePokemon.addEventListener("change", (event) =>{
+const selectPokemon = event.target.value
+if(selectPokemon){
+data.pokemon = filterPokemon(data.pokemon, selectPokemon)
+verType(filterPokemon(data.pokemon, selectPokemon))
+}
+});
