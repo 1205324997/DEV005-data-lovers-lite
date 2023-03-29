b09ff1a73994575ea26cb0eaf4f85109879c049f
1205324997 <josi.i.dami@gmail.com>
2023-03-29 17:10:04 -0500

    test echos

--- test/data.spec.js
+++ test/data.spec.js
@@ -1,23 +1,68 @@
-import { example, anotherExample } from '../src/data.js';
+import { filterPokemon, regionPokemon, sortOrder, reverOrder } from '../src/data.js';
 
+const typePokemon = [{
+  "type": [
+    "grass"
+  ],
+}];
 
-describe('example', () => {
-  it('is a function', () => {
-    expect(typeof example).toBe('function');
+describe('Deberia mostrar los pokemon por tipo', () => {
+  test('Es una function', () => {
+    expect(typeof filterPokemon).toBe('function');
   });
 
-  it('returns `example`', () => {
-    expect(example()).toBe('example');
+  test('Deberia retornarme los pokemon tipo "grass"', () => {
+    expect(filterPokemon(typePokemon, "grass")).toEqual(typePokemon);
   });
 });
 
 
-describe('anotherExample', () => {
-  it('is a function', () => {
-    expect(typeof anotherExample).toBe('function');
+
+const regionPokemo = [{
+  "generation": {
+    "name": "kanto",
+  }
+}];
+
+describe('regionPokemon', () => {
+  test('is a function', () => {
+    expect(typeof regionPokemon).toBe('function');
   });
 
-  it('returns `anotherExample`', () => {
-    expect(anotherExample()).toBe('OMG');
+  test('Deberia retornarme los pokemon "generation[name]"', () => {
+    expect(regionPokemon(regionPokemo, "kanto")).toEqual(regionPokemo);
+  });
+});
+
+const order1 =
+  [{ name: "bulbasaur" },
+    { name: "charmander" },
+    { name: "ivysaur" },
+    { name: "venusaur" }];
+
+describe('deberia ordenar los pokemon de A-Z', () => {
+  test('Es una funcion', () => {
+    expect(typeof sortOrder).toBe('function');
+  });
+
+  test('deberia de retornar un array de objetos por orden alfabetico', () => {
+    expect(sortOrder(order1, "a-z")).toEqual(order1);
+  });
+});
+
+const order2 =
+  [{ name: "bulbasaur" },
+    { name: "charmander" },
+    { name: "ivysaur" },
+    { name: "venusaur" }];
+
+
+describe('deberia ordenar los pokemon de Z-A', () => {
+  test('Es una funcion', () => {
+    expect(typeof reverOrder).toBe('function');
+  });
+
+  test('deberia de retornar un array de objetos por orden alfabetico', () => {
+    expect(reverOrder(order2, "z-a")).toEqual(order2);
   });
 });
